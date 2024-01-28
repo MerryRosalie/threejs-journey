5d932dece7f2e4f9dbb8b70171829042bffd380d
Merry Rosalie <z5391208@ad.unsw.edu.au>
2024-01-28 20:23:59 +0700

    added more rollup options for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const isCodeSandbox =
 
 export default {
   root: "src/",
-  publicDir: "../static/",
+  publicDir: "static/",
   base: "./",
   server: {
     host: true,
@@ -20,12 +20,26 @@ export default {
         main: resolve(__dirname, "src/index.html"),
         "basic-scene": resolve(__dirname, "src/basic-scene/index.html"),
         animations: resolve(__dirname, "src/animations/index.html"),
+        bounce: resolve(__dirname, "src/animations/bounce/index.html"),
+        rotate: resolve(__dirname, "src/animations/rotate/index.html"),
+        timeline: resolve(__dirname, "src/animations/timeline/index.html"),
+        zoom: resolve(__dirname, "src/animations/zoom/index.html"),
         "transform-object": resolve(
           __dirname,
           "src/transform-object/index.html"
         ),
+        group: resolve(__dirname, "src/transform-object/group/index.html"),
+        transform: resolve(
+          __dirname,
+          "src/transform-object/transform/index.html"
+        ),
         cameras: resolve(__dirname, "src/cameras/index.html"),
+        mouse: resolve(__dirname, "src/cameras/mouse/index.html"),
+        orbit: resolve(__dirname, "src/cameras/orbit/index.html"),
+        orthographic: resolve(__dirname, "src/cameras/orthographic/index.html"),
         textures: resolve(__dirname, "src/textures/index.html"),
+        minecraft: resolve(__dirname, "src/textures/minecraft/index.html"),
+        materials: resolve(__dirname, "src/textures/materials/index.html"),
       },
     },
   },
